7eb1b7ccee96a899a993236627f45bf141ebc90f
Haseong Kim <kimh22@uni.coventry.ac.uk>
2019-11-01 23:32:52 +0000

    Recode that users can view index without authentication

--- index.js
+++ index.js
@@ -42,9 +42,6 @@ const dbName = 'website.db'
  */
 router.get('/', async ctx => {
 	try {
-		if(ctx.session.authorised !== true) return ctx.redirect('/login?msg=you need to log in')
-		const data = {}
-		if(ctx.query.msg) data.msg = ctx.query.msg
 		await ctx.render('index')
 	} catch(err) {
 		await ctx.render('error', {message: err.message})
