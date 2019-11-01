3186a8be2ac220a17a1f164cb617dff4da6568c6
Haseong Kim <kimh22@uni.coventry.ac.uk>
2019-11-01 23:46:58 +0000

    Fix a module export error in the router

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -5,7 +5,6 @@ const Router = require('koa-router')
 const koaBody = require('koa-body')({multipart: true, uploadDir: '.'})
 const router = new Router()
 
-/* IMPORT CUSTOM MODULES */
 var UserController = require('../controllers/UserController')
 
 router.get('/', UserController.home)
@@ -14,3 +13,5 @@ router.post('/register', koaBody, UserController.register)
 router.get('/login', UserController.loginForm)
 router.post('/login', UserController.login)
 router.get('/logout', UserController.logout)
+
+module.exports = router
