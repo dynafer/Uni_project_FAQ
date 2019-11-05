6cbe2fb024af7ed90a79ad35d20333c6a4701a53
Haseong Kim <kimh22@uni.coventry.ac.uk>
2019-11-05 00:23:44 +0000

    Fix that the posters (question or answer) can't rate or flag their own questions or answers

--- services/AnswerServices/FlagAnswerService.js
+++ services/AnswerServices/FlagAnswerService.js
@@ -8,17 +8,18 @@ exports.flagAnswer = async function (query) {
         if(query.flagtype === 1 || query.flagtype === 2) {
             var FAQ = await new faqModel("website.db")
             const checkAuthor = await FAQ.getQuestions({faqId: query.faqId})
-            if(checkAuthor.length === 0) throw new Error("No FAQ found")
-            if(checkAuthor[0].authorId !== query.sessionId) throw new Error("No permission")
-            if(checkAuthor[0].solved !== 0) throw new Error("Already Solved")
+            if(checkAuthor.length === 0) throw new Error(`No FAQ found`)
+            if(checkAuthor[0].authorId !== query.sessionId) throw new Error(`No permission`)
+            if(checkAuthor[0].solved !== 0) throw new Error(`Already Solved`)
             const flagAnswer = await FAQ.getAnswers({id: query.answerId})
-            if(flagAnswer.length === 0) throw new Error("No Answer found")
-            if(flagAnswer[0].faqId !== query.faqId) throw new Error("Access in a wrong way")
-            if(flagAnswer[0].flagged !== 0) throw new Error("Already Flagged")
+            if(flagAnswer.length === 0) throw new Error(`No Answer found`)
+            if(flagAnswer[0].faqId !== query.faqId) throw new Error(`Access in a wrong way`)
+            if(flagAnswer[0].flagged !== 0) throw new Error(`Already Flagged`)
+            if(flagAnswer[0].authorId === query.sessionId) throw new Error(`Can't flag your own answer`)
             await FAQ.flagAnswer(query)
             return true
         } else {
-            throw Error("Access in a wrong way")
+            throw Error(`Access in a wrong way`)
         }
     } catch (e) {
         throw e
