c3a7cf84135e2c3a5d7e573a3ed97cd6a250947f
Haseong Kim <kimh22@uni.coventry.ac.uk>
2019-11-07 18:59:37 +0000

    Apply for uncovered line after unit testing, change uncovered lines in getUsers function to throw error and cover it on new unit test

---
+++ unit tests/model.spec.js
@@ -0,0 +1,33 @@
+
+'use strict'
+
+const User = require('../models/User')
+
+/**
+ *  This unit testing will handle only uncovered line on models after testing others
+ *  In addition, not handle the codes following:
+ *  catch(err) {
+ *      throw err
+ *  }
+ */
+
+beforeAll(async () => {
+    const RegisterService = require('../services/UserServices/RegisterService')
+    await RegisterService.register({user: 'doej', pass: 'password', pass2: 'password'})
+})
+describe('getUsers()', () => {
+	test('error if use this method without any parameters', async done => {
+		expect.assertions(1)
+        const user = await new User("website.db")
+        await expect(user.getUsers({}))
+            .rejects.toEqual( Error(`Parameter Error`) )
+		done()
+	})
+})
+
+
+afterAll(async () => {
+    const sqlite = require('sqlite-async')
+    const db = await sqlite.open("website.db")
+    await db.run("DROP TABLE users;")
+})
