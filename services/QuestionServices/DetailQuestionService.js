3777f7d9e446a85286e4c5c1aff85ce87e94757d
Haseong Kim <kimh22@uni.coventry.ac.uk>
2019-11-19 23:22:17 +0000

    Make Detail Question Service increase quality of code

--- services/QuestionServices/DetailQuestionService.js
+++ services/QuestionServices/DetailQuestionService.js
@@ -1,27 +1,29 @@
+/*eslint linebreak-style: ["error", "windows"]*/
 
 'use strict'
 
-var userModel = require('../../models/User')
-var faqModel = require('../../models/FAQ')
+const userModel = require('../../models/User'),
+	faqModel = require('../../models/FAQ'),
+	func = require('../../function'),
+	dbName = 'website.db'
 
-exports.detailsQuestion = async function (query) {
-    try {
-        if(query.faqId === 0 || query.faqId === null || query.faqId === undefined) throw Error(`Error during getting information of the question`)
-        var FAQ = await new faqModel("website.db")
-        var User = await new userModel("website.db")
-        const details = await FAQ.getQuestions(query)
-        if(details.nolist === undefined) {
-            if(details[0].imageBool === 1) {
-                const imageData = await FAQ.QuestionThumbnail({id: details[0].id})
-                details[0].imageData = imageData
-            }
-            const getAuthorName = await User.getUsers({userid: details[0].authorId})
-            details[0].author = getAuthorName[0].user
-        } else {
-            throw Error(`Can't find the question`)
-        }
-        return details[0]
-    } catch (e) {
-        throw e
-    }
+exports.detailsQuestion = async query => {
+	try {
+		func.mustHaveParameter([{variable: query.faqId, numberOrlength: query.faqId}])
+		const FAQ = await new faqModel(dbName), User = await new userModel(dbName)
+		const details = await FAQ.getQuestions(query)
+		if(details.nolist === undefined) {
+			if(details[0].imageBool === 1) {
+				const imageData = await FAQ.QuestionThumbnail({id: details[0].id})
+				details[0].imageData = imageData
+			}
+			const getAuthorName = await User.getUsers({userid: details[0].authorId})
+			details[0].author = getAuthorName[0].user
+		} else {
+			throw Error('Can\'t find the question')
+		}
+		return details[0]
+	} catch (e) {
+		throw e
+	}
 }
