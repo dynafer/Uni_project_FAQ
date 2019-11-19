73f32f4d81b062666f551d1955bd4b53cc3d670c
Haseong Kim <kimh22@uni.coventry.ac.uk>
2019-11-19 22:51:20 +0000

    Make the answer list service increase quality of code

--- function.js
+++ function.js
@@ -26,4 +26,62 @@ function isNotNull(variable, numberOrlength) {
 	}
 }
 
-module.exports = { isLoggedin, isZeroSizeOfImage, isNotNull }
+function getRateAverage(getAnswerRate) {
+	let averageRate = 0.0
+	if(getAnswerRate.nolist === undefined) {
+		for(let l=0; l<getAnswerRate.length; l++) {
+			averageRate = averageRate + getAnswerRate[l].rate
+		}
+		averageRate = (averageRate / getAnswerRate.length).toFixed(1)
+	} else {
+		averageRate = 0.0
+	}
+	return averageRate
+}
+
+function beforeGetRateStar(averageRate) {
+	const boolZero = averageRate === 0 || averageRate === 0.0 ? true : false
+	averageRate = averageRate.split('.')
+	averageRate[0] = parseInt(averageRate[0])
+	averageRate[1] = parseInt(averageRate[1])
+	return {data: averageRate, boolZero: boolZero}
+}
+
+function getZeroRateStar() {
+	let ratedHTMLstars = ''
+	const maxStar = 5
+	for(let star = 0; star < maxStar; star++) {
+		ratedHTMLstars += '<span class="full"></span>'
+	}
+	return ratedHTMLstars
+}
+
+function getRateStar(averageRate) {
+	let ratedHTMLstars = ''
+	const maxRate = 5.0
+	while(averageRate[0] > 0) {
+		ratedHTMLstars += '<span class="full checked"></span>'
+		averageRate[0]--
+	}
+	if(averageRate[1] !== 0) {
+		ratedHTMLstars += '<span class="half checked"></span><span class="full"></span>'
+		averageRate[1] = 0
+	}
+	for(let getRest = Math.floor(maxRate - averageRate); getRest > 0; getRest--) {
+		ratedHTMLstars += '<span class="full"></span>'
+	}
+	return ratedHTMLstars
+}
+
+function getRateStarHTML(averageRate) {
+	const tempAverageRate = beforeGetRateStar(averageRate)
+	let averageRateHTML
+	if(tempAverageRate.boolZero === true) {
+		averageRateHTML = getZeroRateStar()
+	} else {
+		averageRateHTML = getRateStar(tempAverageRate.data)
+	}
+	return averageRateHTML
+}
+
+module.exports = { isLoggedin, isZeroSizeOfImage, isNotNull, getRateAverage, getRateStarHTML }
--- services/AnswerServices/AnswerListService.js
+++ services/AnswerServices/AnswerListService.js
@@ -1,65 +1,28 @@
-
+/*eslint linebreak-style: ["error", "windows"]*/
 'use strict'
 
-var userModel = require('../../models/User')
-var faqModel = require('../../models/FAQ')
+const userModel = require('../../models/User'),
+	faqModel = require('../../models/FAQ'),
+	func = require('../../function'),
+	dbName = 'website.db'
 
-exports.getAnswers = async function (query) {
-    try {
-        var FAQ = await new faqModel("website.db")
-        var User = await new userModel("website.db")
-		var list = await FAQ.getAnswers(query)
-		if(list.nolist === undefined) {
-			for(var i=0; i<list.length; i++) {
-				const getAuthorName = await User.getUsers({userid: list[i].authorId})
-				list[i].author = getAuthorName[0].user
-				const getAnswerRate = await FAQ.getAnswerRates({answerId: list[i].id})
-				const getQuestion = await FAQ.getQuestions({faqId: query.faqId})
-				if(getQuestion.nolist !== undefined) throw Error(`The question doesn't exist`)
-				var average_rate = 0.0
-				if(getAnswerRate.nolist === undefined) {
-					for(var l=0; l<getAnswerRate.length; l++) {
-						average_rate = average_rate + getAnswerRate[l].rate
-					}
-					if(average_rate !== 0.0) average_rate = (average_rate / getAnswerRate.length).toFixed(1)
-				} else {
-					average_rate = 0.0
-				}
-				let tempRate = String(average_rate)
-				var ratedHTMLstars = ''
-				if(average_rate === 0) {
-					for(var star = 0; star < 5; star++) {
-						ratedHTMLstars = ratedHTMLstars + '<span class="full"></span>'
-					}
-				} else {
-					tempRate = tempRate.split('.')
-					tempRate[0] = parseInt(tempRate[0])	// to make string to integer
-					tempRate[1] = parseInt(tempRate[1])	// to make string to integer
-					while(tempRate[0] > 0) {
-						ratedHTMLstars = ratedHTMLstars + '<span class="full checked"></span>'
-						tempRate[0]--;
-					}
-					if(tempRate[1] !== 0) {
-						ratedHTMLstars = ratedHTMLstars + '<span class="half checked"></span><span class="full"></span>'
-						tempRate[1] = 0
-					}
-					let getRest = Math.floor(5.0 - average_rate)
-					while(getRest !== 0) {
-						ratedHTMLstars = ratedHTMLstars + '<span class="full"></span>'
-						getRest--;
-					}
-				}
-				list[i].averageRate = ratedHTMLstars
-				if(query.sessionid !== 0 && query.sessionid !== null && query.sessionid !== undefined) {
-					list[i].sessionid = query.sessionid
-				}
-				
-				list[i].questionAuthor = getQuestion[0].authorId
-				list[i].solved = getQuestion[0].solved
-			}
+exports.getAnswers = async query => {
+	try {
+		const FAQ = await new faqModel(dbName), User = await new userModel(dbName), list = await FAQ.getAnswers(query)
+		for(let i=0; i<list.length; i++) {
+			const getAuthorName = await User.getUsers({userid: list[i].authorId}),
+				getAnswerRate = await FAQ.getAnswerRates({answerId: list[i].id}),
+				getQuestion = await FAQ.getQuestions({faqId: query.faqId})
+			list[i].author = getAuthorName[0].user
+			if(getQuestion.nolist !== undefined) throw Error('The question doesn\'t exist')
+			const averageRate = String(func.getRateAverage(getAnswerRate))
+			list[i].averageRate = func.getRateStarHTML(averageRate)
+			list[i].sessionid = func.isNotNull(query.sessionid, query.sessionid) === true ? query.sessionid : 0
+			list[i].questionAuthor = getQuestion[0].authorId
+			list[i].solved = getQuestion[0].solved
 		}
-        return list;
-    } catch (e) {
-        throw e
-    }
+		return list
+	} catch (e) {
+		throw e
+	}
 }
