8a9242a5914adeb515ab3f6cc6ef8d0aac7cd512
Haseong Kim <kimh22@uni.coventry.ac.uk>
2019-11-20 00:01:01 +0000

    The code quality of Contribute Service is gone up

--- services/UserServices/ContributeService.js
+++ services/UserServices/ContributeService.js
@@ -1,22 +1,25 @@
+/*eslint linebreak-style: ["error", "windows"]*/
 
 'use strict'
 
-var userModel = require('../../models/User')
+const userModel = require('../../models/User'),
+	func = require('../../function/function.general'),
+	dbName = 'website.db'
 
-exports.contribute = async function (query) {
-    try {
-        if(query.userId === 0 || query.userId === null || query.userId === undefined) throw Error(`You don't login yet`)
-        var User = await new userModel("website.db")
-        const getUser = await User.getUsers({userid: query.userId})
-        if(getUser.length === 0) throw Error(`Error during contributing`)
-        if((getUser[0].contribution + query.contribution) <= 0) {
-            query.contribution = 0
-        } else {
-            query.contribution = getUser[0].contribution + query.contribution
-        }
-        var contribute = await User.contribute(query)
-        return contribute;
-    } catch (e) {
-        throw e
-    }
+exports.contribute = async query => {
+	try {
+		func.isLoggedin(query.userId)
+		const User = await new userModel(dbName),
+		    getUser = await User.getUsers({userid: query.userId})
+		if(getUser.length === 0) throw Error('Error during contributing')
+		if((getUser[0].contribution + query.contribution) <= 0) {
+			query.contribution = 0
+		} else {
+			query.contribution = getUser[0].contribution + query.contribution
+		}
+		const contribute = await User.contribute(query)
+		return contribute
+	} catch (e) {
+		throw e
+	}
 }
