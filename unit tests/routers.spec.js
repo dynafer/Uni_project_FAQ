a96f2aad1209cec4f8f696ff2baf29be93b524c8
Haseong Kim <kimh22@uni.coventry.ac.uk>
2019-11-23 21:40:25 +0000

    Implement FAQRouter in routers.spec.js and delete never used lines in the controller

--- unit tests/routers.spec.js
+++ unit tests/routers.spec.js
@@ -2,26 +2,29 @@
 'use strict'
 
 const request = require('supertest'),
+	session = require('supertest-session'),
 	server = require('../index'),
 	sqlite = require('sqlite-async'),
 	fs = require('fs')
 
 describe('UserRouter()', () => {
 	test('GET /', async done => {
-		expect.assertions(3)
+		expect.assertions(4)
 		const response = await request(server).get('/')
 		expect(response.status).toBe(200)
 		expect(response.statusCode).toBe(200)
 		expect(response.ok).toBe(true)
+		expect(response.text.includes('An Error Has Occurred')).toBe(false)
 		done()
 	})
 
 	test('GET /register', async done => {
-		expect.assertions(3)
+		expect.assertions(4)
 		const response = await request(server).get('/register')
 		expect(response.status).toBe(200)
 		expect(response.statusCode).toBe(200)
 		expect(response.ok).toBe(true)
+		expect(response.text.includes('An Error Has Occurred')).toBe(false)
 		done()
 	})
 
@@ -58,11 +61,12 @@ describe('UserRouter()', () => {
 	})
 
 	test('GET /login', async done => {
-		expect.assertions(3)
+		expect.assertions(4)
 		const response = await request(server).get('/login')
 		expect(response.status).toBe(200)
 		expect(response.statusCode).toBe(200)
 		expect(response.ok).toBe(true)
+		expect(response.text.includes('An Error Has Occurred')).toBe(false)
 		done()
 	})
 
@@ -93,11 +97,205 @@ describe('UserRouter()', () => {
 	})
 
 	test('GET /ranking', async done => {
-		expect.assertions(3)
+		expect.assertions(4)
 		const response = await request(server).get('/ranking')
 		expect(response.status).toBe(200)
 		expect(response.statusCode).toBe(200)
 		expect(response.ok).toBe(true)
+		expect(response.text.includes('An Error Has Occurred')).toBe(false)
+		done()
+	})
+
+})
+
+describe('FAQRouter()', () => {
+
+	const sessionserver = session(server)
+	let loggedin
+
+	beforeAll((done) => {
+		sessionserver.post('/login')
+			.send('user=test')
+			.send('pass=test')
+			.expect(302)
+			.end(async() => {
+				loggedin = sessionserver
+				return done()
+			})
+	})
+
+	test('GET /list with an empty list', async done => {
+		expect.assertions(4)
+		const response = await loggedin.get('/list')
+		expect(response.status).toBe(200)
+		expect(response.statusCode).toBe(200)
+		expect(response.ok).toBe(true)
+		expect(response.text.includes('An Error Has Occurred')).toBe(false)
+		done()
+	})
+
+	test('GET /writeQuestion', async done => {
+		expect.assertions(4)
+		const response = await loggedin.get('/writeQuestion')
+		expect(response.status).toBe(200)
+		expect(response.statusCode).toBe(200)
+		expect(response.ok).toBe(true)
+		expect(response.text.includes('An Error Has Occurred')).toBe(false)
+		done()
+	})
+
+	test('POST /writeQuestion with an empty picture', async done => {
+		expect.assertions(2)
+		const response = await loggedin.post('/writeQuestion')
+			.field('title', 'This is the first test')
+			.field('description', 'This is a description')
+			.attach('image', './unit tests/assets/image/testSizezero.png')
+		expect(response.status).toBe(302)
+		expect(response.redirect).toBe(true)
+		done()
+	})
+
+	test('POST /writeQuestion with a picture', async done => {
+		expect.assertions(2)
+		const response = await loggedin.post('/writeQuestion')
+			.field('title', 'This is the second test')
+			.field('description', 'This is a description')
+			.attach('image', './unit tests/assets/image/testPicture.png')
+		expect(response.status).toBe(302)
+		expect(response.redirect).toBe(true)
+		done()
+	})
+
+	test('POST /writeQuestion error', async done => {
+		expect.assertions(1)
+		const response = await loggedin.post('/writeQuestion')
+			.field('title', '')
+		expect(response.text.includes('An Error Has Occurred')).toBe(true)
+		done()
+	})
+
+	test('GET /faq/:id([0-9]{1,}) without answers', async done => {
+		expect.assertions(4)
+		const response = await loggedin.get('/faq/2')
+		expect(response.status).toBe(200)
+		expect(response.statusCode).toBe(200)
+		expect(response.ok).toBe(true)
+		expect(response.text.includes('An Error Has Occurred')).toBe(false)
+		done()
+	})
+
+	test('GET /faq/:id([0-9]{1,}) with answers', async done => {
+		expect.assertions(4)
+		await loggedin.post('/faq/1')
+			.field('answerInput', 'This%20is%20a%20description')
+		const response = await loggedin.get('/faq/1')
+		expect(response.status).toBe(200)
+		expect(response.statusCode).toBe(200)
+		expect(response.ok).toBe(true)
+		expect(response.text.includes('An Error Has Occurred')).toBe(false)
+		done()
+	})
+
+	test('GET /faq/:id([0-9]{1,}) error', async done => {
+		expect.assertions(1)
+		const response = await loggedin.get('/faq/4')
+		expect(response.text.includes('An Error Has Occurred')).toBe(true)
+		done()
+	})
+
+	test('GET /FullImage/:id([0-9]{1,})', async done => {
+		expect.assertions(4)
+		const response = await loggedin.get('/FullImage/2?type=png')
+		expect(response.status).toBe(200)
+		expect(response.statusCode).toBe(200)
+		expect(response.ok).toBe(true)
+		expect(response.text.includes('An Error Has Occurred')).toBe(false)
+		done()
+	})
+
+	test('GET /FullImage/:id([0-9]{1,}) error', async done => {
+		expect.assertions(1)
+		const response = await loggedin.get('/FullImage/2')
+		expect(response.text.includes('An Error Has Occurred')).toBe(true)
+		done()
+	})
+
+	test('POST /faq/:id([0-9]{1,}) write answer', async done => {
+		expect.assertions(2)
+		await loggedin.destroy()
+		await sessionserver.post('/login')
+			.send('user=test1')
+			.send('pass=test1')
+			.expect(302)
+			.then(loggedin = sessionserver)
+		const response = await loggedin.post('/faq/2')
+			.field('answerInput', 'This%20is%20a%20description')
+		expect(response.status).toBe(302)
+		expect(response.redirect).toBe(true)
+		done()
+	})
+
+	test('POST /faq/:id([0-9]{1,}) write answer error', async done => {
+		expect.assertions(1)
+		const response = await loggedin.post('/faq/2')
+			.field('answerInput', '')
+		expect(response.text.includes('An Error Has Occurred')).toBe(true)
+		done()
+	})
+
+	test('GET /faq/:id([0-9]{1,})/:answerid([0-9]{1,})/:flagtype([0-9]{1,}) solved', async done => {
+		expect.assertions(2)
+		await loggedin.post('/faq/1')
+			.field('answerInput', 'This%20is%20a%20description')
+		await loggedin.destroy()
+		await sessionserver.post('/login')
+			.send('user=test')
+			.send('pass=test')
+			.expect(302)
+			.then(loggedin = sessionserver)
+		const response = await loggedin.get('/faq/2/2/1')
+		expect(response.status).toBe(302)
+		expect(response.redirect).toBe(true)
+		done()
+	})
+
+	test('GET /faq/:id([0-9]{1,})/:answerid([0-9]{1,})/:flagtype([0-9]{1,}) inappropriate', async done => {
+		expect.assertions(2)
+		const response = await loggedin.get('/faq/1/3/2')
+		expect(response.status).toBe(302)
+		expect(response.redirect).toBe(true)
+		done()
+	})
+
+	test('GET /faq/:id([0-9]{1,})/:answerid([0-9]{1,})/:flagtype([0-9]{1,}) error', async done => {
+		expect.assertions(1)
+		const response = await loggedin.get('/faq/2/5/1')
+		expect(response.text.includes('An Error Has Occurred')).toBe(true)
+		done()
+	})
+
+	test('GET /faq/:id([0-9]{1,})/:answerid([0-9]{1,}) rate an answer', async done => {
+		expect.assertions(2)
+		const response = await loggedin.get('/faq/2/2?rate=4')
+		expect(response.status).toBe(302)
+		expect(response.redirect).toBe(true)
+		done()
+	})
+
+	test('GET /faq/:id([0-9]{1,})/:answerid([0-9]{1,}) rate an answer error', async done => {
+		expect.assertions(1)
+		const response = await loggedin.get('/faq/2/2?rate=6')
+		expect(response.text.includes('An Error Has Occurred')).toBe(true)
+		done()
+	})
+
+	test('GET /list with at least one question', async done => {
+		expect.assertions(4)
+		const response = await loggedin.get('/list')
+		expect(response.status).toBe(200)
+		expect(response.statusCode).toBe(200)
+		expect(response.ok).toBe(true)
+		expect(response.text.includes('An Error Has Occurred')).toBe(false)
 		done()
 	})
 
@@ -105,7 +303,11 @@ describe('UserRouter()', () => {
 		await server.close(server.removeAllListeners())
 		const db = await sqlite.open('website.db')
 		await db.run('DROP TABLE users;')
+		await db.run('DROP TABLE questions;')
+		await db.run('DROP TABLE questionThumbnails;')
+		await db.run('DROP TABLE answers;')
+		await db.run('DROP TABLE answersRate;')
 		fs.unlinkSync('public/avatars/test1.png')
+		fs.unlinkSync('public/upload/FAQ/2.png')
 	})
-
 })
